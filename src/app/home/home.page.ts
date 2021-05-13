710bf76681460d1489486d37f924adc535e7af6d
HenriqueHartmann <alemaohartmann@gmail.com>
2021-05-13 01:57:11 -0300

    Project complete

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FotoService } from './../services/foto.service';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public fotoService : FotoService,
+              public actionSheetController : ActionSheetController) {}
+
+  async presentActionSheet(index : number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'O que deseja fazer?',
+      buttons: [{
+        text: 'Delete',
+        icon: 'trash',
+        handler: () => {
+          this.fotoService.deleteFoto(index);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel'
+      }]
+    });
+    await actionSheet.present();
+  }
+
+}
---
+++ src/app/services/foto.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+
+const { Camera } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FotoService {
+
+  public fotos : Array<Foto> = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType : CameraResultType.Uri,
+      source : CameraSource.Camera,
+      quality : 100
+    });
+    this.fotos.unshift({
+      webviewPath : capturedPhoto.webPath,
+    });
+  }
+
+  public async deleteFoto(index : number) {
+    this.fotos.splice(index, 1);
+  }
+}
+
+export interface Foto {
+  webviewPath : string,
+  base64 ?: string
+}
